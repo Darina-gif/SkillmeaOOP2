23c5c0469c1ed43e3e11bfa241fbbe3ad19bf172
Darina-gif <darinkabb@gmail.com>
2025-06-03 21:14:10 +0200

    main

---
+++ src/RodinnyDum.java
@@ -0,0 +1,13 @@
+public class RodinnyDum {
+    int pocetPoschodi;
+    String material;
+    float rozloha;
+    int pocetOken;
+
+    public void vypocetCenyDomu(int cenaZaMetr, String jmenoRodiny){
+        float cena = rozloha * cenaZaMetr * pocetPoschodi;
+        System.out.println(jmenoRodiny+ " " + cena);
+
+    }
+
+}
